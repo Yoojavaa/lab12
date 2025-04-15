e10bcc58ee70b1ea9b0991258d962674455efc69
ken lester bernaldez <kenlesterbernaldez24@gmail.com>
2025-04-15 15:10:18 +0800

    final

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,32 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
  
  
-Route::get('/register', [AuthController::class, 'showRegister']);
-Route::post('/register', [AuthController::class, 'register']);
- 
- 
-Route::get('/login', [AuthController::class, 'showLogin']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('guest')->group(function () {
+
+  Route::get('/register', [AuthController::class, 'showRegister']);
+
+  Route::post('/register', [AuthController::class, 'register']);
+
+
+
+  Route::get('/login', [AuthController::class, 'showLogin']);
+
+  Route::post('/login', [AuthController::class, 'login']);
+
+});
+
  
  
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
  
  
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('auth.dashboard');
 })->middleware('auth');
+
+
+
+
+
+
+
